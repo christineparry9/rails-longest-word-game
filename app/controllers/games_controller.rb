52dfe2a67a3c9aee0a3a1594df9d2b4221e94a64
Christine Parry <christine.parry9@gmail.com>
2022-11-21 17:40:55 +0100

    .

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,10 +7,12 @@ class GamesController < ApplicationController
   @letters = ('a'..'z').to_a.shuffle[0..9]
   end
 
-  # def included?
-  #   word = params[:word]
-  #   word.chars.all? { |letter| word.count(letter) <= @letters.count(letter) }
-  # end
+
+  def included
+    @letters = params[:letters]
+    word = params[:word]
+    word.chars.all? { |letter| word.count(letter) <= @letters.count(letter) }
+  end
 
 
   def score
@@ -20,8 +22,10 @@ class GamesController < ApplicationController
     hash = JSON.parse(serialized)
     if hash['found'] != true
       @result = "Sorry but #{@word} does not seem to be an English word."
-    elsif hash['found'] = true
+    elsif hash['found'] = true && included
       @result = "Congratulations"
+    elsif hash['found'] = true && !included
+      @result = "you used the wrong letters"
     end
 
   end
